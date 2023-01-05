4ed414138dabb67bff707b345772edff0ad5a778
zjl <1362913430@qq.com>
2023-01-05 12:02:58 +0800

    Site updated: 2023-01-05 12:02:58

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+
+
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "18",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "10",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "fca7a7eac38e4e5abcb2a42092642e2b"
+  }
+}
